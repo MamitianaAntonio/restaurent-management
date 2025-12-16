dcd9ea574be6c035f2418e6b78eb93e603bf363e
Mamitiana Antonio <ratolojanaharyantonio1@gmail.com>
2025-12-16 14:15:37 +0300

    sql : write the schema of database

---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,20 @@
+CREATE TYPE category_enum AS ENUM ('VEGETABLE', 'ANIMAL', 'MARINE', 'DAIRY', 'OTHER');
+CREATE TYPE dish_type_enum AS ENUM ('START', 'MAIN', 'DESSERT');
+
+-- create table ingredient
+CREATE TABLE Ingredient (
+    id int PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    price numeric(10,2),
+    category category_enum,
+    id_dish int,
+    CONSTRAINT fk_ingredient
+        FOREIGN KEY (id_dish) REFERENCES Dish(id) ON DELETE CASCADE
+);
+
+-- create table dish
+CREATE TABLE Dish (
+    id int PRIMARY KEY NOT NULL,
+    name varchar(255) NOT NULL,
+    dish_type dish_type_enum
+);
