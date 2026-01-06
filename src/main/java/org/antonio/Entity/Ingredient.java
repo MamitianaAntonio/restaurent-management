0207b52207fd184161c93d392590967f481883df
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-01-06 16:02:16 +0300

    feat : implement findDishById methods

--- src/main/java/org/antonio/Entity/Ingredient.java
+++ src/main/java/org/antonio/Entity/Ingredient.java
@@ -1,24 +1,20 @@
 package org.antonio.Entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Ingredient {
   private int id;
   private String name;
   private Double price;
   private CategoryEnum category;
   private Dish dish;
-
-  public Ingredient(int id, String name, Double price, CategoryEnum category, Dish dish) {
-    this.id = id;
-    this.name = name;
-    this.price = price;
-    this.category = category;
-    this.dish = dish;
-  }
 
   // get dish name
   public String getDishName () {
