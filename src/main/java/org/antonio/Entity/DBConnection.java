edd34c0f4864882ffe04f8aa4f9f7eb584afe883
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-01-06 14:51:56 +0300

    feat : add dotenv dependencies and change DBConnection feature

--- src/main/java/org/antonio/Entity/DBConnection.java
+++ src/main/java/org/antonio/Entity/DBConnection.java
@@ -1,15 +1,18 @@
 package org.antonio.Entity;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-  private static final String URL = "jdbc:postgresql://localhost:5432/mini_dish_db";
-  private static final String USERNAME = "mini_dish_db_manager";
-  private static final String PASSWORD = "";
+  private static final Dotenv dotenv = Dotenv.load();
+  private static final String JDBC_URL = dotenv.get("JDBC_URL");
+  private static final String USERNAME = dotenv.get("USERNAME");
+  private static final String PASSWORD = dotenv.get("PASSWORD");
 
   public static Connection getConnection() throws SQLException {
-    return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
   }
 }
