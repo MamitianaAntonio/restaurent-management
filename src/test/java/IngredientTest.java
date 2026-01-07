643b7dd2fd7e525d31a1c41b2406b47c1c17576d
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-01-07 10:42:56 +0300

    feat : write findIngredientsByCriteria and all of test

--- src/test/java/IngredientTest.java
+++ src/test/java/IngredientTest.java
@@ -2,11 +2,9 @@ import org.antonio.Entity.CategoryEnum;
 import org.antonio.Entity.DataRetriever;
 import org.antonio.Entity.Dish;
 import org.antonio.Entity.Ingredient;
-import org.checkerframework.checker.units.qual.A;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
-import java.math.BigDecimal;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -14,7 +12,7 @@ public class IngredientTest {
   private DataRetriever dataRetriever = new DataRetriever();
 
   @Test
-  public void findIngredient_page2size2_shouldReturnPouletAndChocolat () throws SQLException {
+  public void findIngredient_page2size2_shouldReturnPouletAndChocolat() throws SQLException {
     int page = 2;
     int size = 2;
 
@@ -38,7 +36,7 @@ public class IngredientTest {
   }
 
   @Test
-  public void createIngredients_shouldReturnFromageAndOignon () throws SQLException {
+  public void createIngredients_shouldReturnFromageAndOignon() throws SQLException {
     List<Ingredient> newIngredients = List.of(
         new Ingredient("Fromage", CategoryEnum.DAIRY, 1200.0),
         new Ingredient("Oignon", CategoryEnum.VEGETABLE, 500.0)
@@ -55,7 +53,7 @@ public class IngredientTest {
   }
 
   @Test(expected = RuntimeException.class)
-  public void createIngredients_shouldThrowException_whenDuplicateIngredient() throws SQLException{
+  public void createIngredients_shouldThrowException_whenDuplicateIngredient() throws SQLException {
     List<Ingredient> newIngredients = List.of(
         new Ingredient("Carotte", CategoryEnum.VEGETABLE, 2000.0),
         new Ingredient("Laitue", CategoryEnum.VEGETABLE, 2000.0)
@@ -69,5 +67,54 @@ public class IngredientTest {
     List<Dish> result = dataRetriever.findDishesByIngredientName("eur");
     Assertions.assertEquals(1, result.size());
     Assertions.assertEquals("Gâteau au chocolat", result.get(0).getName());
+  }
+
+  // Test f) Filtre par catégorie VEGETABLE
+  @Test
+  public void findIngredientsByCriteria_shouldReturnLaitueAndTomate() throws Exception {
+    // GIVEN
+    String ingredientName = null;
+    CategoryEnum category = CategoryEnum.VEGETABLE;
+    String dishName = null;
+    int page = 1;
+    int size = 10;
+
+    // WHEN
+    List<Ingredient> result = dataRetriever.findIngredientsByCriteria(ingredientName, category, dishName, page, size);
+
+    // THEN
+    Assertions.assertNotNull(result);
+    Assertions.assertEquals(2, result.size());
+    Assertions.assertTrue(result.stream().anyMatch(i -> i.getName().equals("Laitue")));
+    Assertions.assertTrue(result.stream().anyMatch(i -> i.getName().equals("Tomate")));
+  }
+
+  // Test g) Pas de résultat attendu
+  @Test
+  public void findIngredientsByCriteria_shouldReturnEmptyList() throws Exception {
+    String ingredientName = "cho";
+    CategoryEnum category = null;
+    String dishName = "Sal";
+    int page = 1;
+    int size = 10;
+
+    List<Ingredient> result = dataRetriever.findIngredientsByCriteria(ingredientName, category, dishName, page, size);
+
+    Assertions.assertNotNull(result);
+    Assertions.assertTrue(result.isEmpty());
+  }
+
+  @Test
+  public void findIngredientsByCriteria_shouldReturnChocolat() throws Exception {
+    String ingredientName = "cho";
+    CategoryEnum category = null;
+    String dishName = "gâteau";
+    int page = 1;
+    int size = 10;
+
+    List<Ingredient> result = dataRetriever.findIngredientsByCriteria(ingredientName, category, dishName, page, size);
+
+    Assertions.assertNotNull(result);
+    Assertions.assertEquals(1, result.size());
   }
 }
